d3ac9d5ff6487966cca11f1c231bf85d5a737416
Yann Klein <yann.klein@me.com>
2019-08-15 23:36:18 +0900

    final word for new

--- app/controllers/holograms_controller.rb
+++ app/controllers/holograms_controller.rb
@@ -57,7 +57,7 @@ class HologramsController < ApplicationController
   def create_raw_qrcode(new_holo_id)
     # Produce the hologram live url
     live_url = "#{root_url}/holograms/#{new_holo_id}/live"
-    @my_live_url = live_url
+    live_url = live_url.sub("http:", "https:")
     # Create the QR code PGN image
     barcode = Barby::QrCode.new(live_url, level: :q, size: 5)
     Base64.encode64(barcode.to_png(xdim: 5))
