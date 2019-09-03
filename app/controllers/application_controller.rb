5b01141f32ef0e7c83f75338bdcbcce829907961
Yann Klein <yann.klein@me.com>
2019-09-03 22:31:45 +0900

    styling, meta for social media, icons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname, :picture, :bio])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname, :picture, :bio])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
