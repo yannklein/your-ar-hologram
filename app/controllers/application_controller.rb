885d97ab50780623ef2e544accbaf52458ab44b6
Yann Klein <yann.klein@me.com>
2019-09-04 00:37:37 +0900

    correction base url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  force_ssl if: :ssl_configured?
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname, :picture, :bio])
@@ -11,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  def ssl_configured?
+    !Rails.env.development?
   end
 end
