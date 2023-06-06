7fa67ce5cdc5a516df0d213ee0a3317bc9f5fcd8
Yann Klein <yann.klein@me.com>
2023-06-06 15:03:53 +0900

    change model to point a one-db-to-rule-them all column names

--- app/models/hologram.rb
+++ app/models/hologram.rb
@@ -1,5 +1,6 @@
 class Hologram < ApplicationRecord
-  belongs_to :user
+  self.table_name = "holobo_holograms"
+  belongs_to :user, foreign_key: "holobo_user_id"
   mount_uploader :video, MediaUploader
   mount_uploader :depth_img, MediaUploader
 
@@ -12,6 +13,6 @@ class Hologram < ApplicationRecord
   end
 
   def self.next_id
-    1 + ActiveRecord::Base.connection.execute("select last_value from holograms_id_seq").first["last_value"]
+    1 + ActiveRecord::Base.connection.execute("select last_value from holobo_holograms_id_seq").first["last_value"]
   end
 end
